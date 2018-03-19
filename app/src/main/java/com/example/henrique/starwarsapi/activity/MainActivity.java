9a77a507dc394f5cac088e265f7e1437ec0a7229
Henrique Menezes <Henriquespmenezes@outlook.com>
2018-03-19 17:48:27 -0300

    -CameraAdded

--- app/src/main/java/com/example/henrique/starwarsapi/activity/MainActivity.java
+++ app/src/main/java/com/example/henrique/starwarsapi/activity/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.henrique.starwarsapi.activity;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.henrique.starwarsapi.R;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    public static final String ALLOW_KEY = "ALLOWED";
+    public static final String CAMERA_PREF = "camera_pref";
 
     private Button buttonPlanet;
     private Button buttonPeople;
@@ -24,19 +40,22 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       buttonPeople = (Button) findViewById(R.id.button1);
-       buttonPlanet = (Button) findViewById(R.id.button2);
-       buttonSpecies = (Button) findViewById(R.id.button3);
-       navigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
-       navigationView.setOnNavigationItemSelectedListener(this);
+        buttonPeople = (Button) findViewById(R.id.button1);
+        buttonPlanet = (Button) findViewById(R.id.button2);
+        buttonSpecies = (Button) findViewById(R.id.button3);
+        navigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+        buttonPeople = (Button) findViewById(R.id.button1);
+        buttonPlanet = (Button) findViewById(R.id.button2);
+        buttonSpecies = (Button) findViewById(R.id.button3);
 
         buttonPeople.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent people = new Intent(MainActivity.this, PeopleActivity.class);
-                startActivity(people);
-
+                Intent intent = new Intent(MainActivity.this, PeopleActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -44,9 +63,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             @Override
             public void onClick(View v) {
 
-                Intent planet = new Intent(MainActivity.this, PlanetsActivity.class);
-                startActivity(planet);
-
+                Intent intent = new Intent(MainActivity.this, PlanetsActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -54,29 +72,149 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             @Override
             public void onClick(View v) {
 
-                Intent specie = new Intent(MainActivity.this, SpeciesActivity.class);
-                startActivity(specie);
-
+                Intent intent = new Intent(MainActivity.this, SpeciesActivity.class);
+                startActivity(intent);
             }
         });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (getFromPref(this, ALLOW_KEY)) {
+                showSettingsAlert();
+            } else if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA)
+
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.CAMERA)) {
+                    showAlert();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CAMERA},
+                            MY_PERMISSIONS_REQUEST_CAMERA);
+                }
+            }
+        }
 
     }
+    public static void saveToPreferences(Context context, String key, Boolean allowed) {
+        SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = myPrefs.edit();
+        prefsEditor.putBoolean(key, allowed);
+        prefsEditor.commit();
+    }
+    public static Boolean getFromPref(Context context, String key) {
+        SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
+                Context.MODE_PRIVATE);
+        return (myPrefs.getBoolean(key, false));
+    }
+    private void showAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("App needs to access the Camera.");
 
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "ALLOW",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                });
+        alertDialog.show();
+    }
+    private void showSettingsAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("App needs to access the Camera.");
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        //finish();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SETTINGS",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        startInstalledAppDetailsActivity(MainActivity.this);
+                    }
+                });
+        alertDialog.show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                for (int i = 0, len = permissions.length; i < len; i++) {
+                    String permission = permissions[i];
+
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        boolean
+                                showRationale =
+                                ActivityCompat.shouldShowRequestPermissionRationale(
+                                        this, permission);
+                        if (showRationale) {
+                            showAlert();
+                        } else if (!showRationale) {
+                            saveToPreferences(MainActivity.this, ALLOW_KEY, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    public static void startInstalledAppDetailsActivity(final Activity context) {
+        if (context == null) {
+            return;
+        }
+        final Intent i = new Intent();
+        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + context.getPackageName()));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        context.startActivity(i);
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.iconPrincipal:{
-                    Intent intentPrincipal = new Intent(MainActivity.this, MainActivity.class);
-                    finish();
-                    startActivity(intentPrincipal);
-                    break;
-            }
-            case R.id.iconCamera: {
-                Intent intentPrincipal = new Intent(MainActivity.this, CameraActivity.class);
+                Intent intentPrincipal = new Intent(MainActivity.this, MainActivity.class);
+                finish();
                 startActivity(intentPrincipal);
                 break;
             }
-
+            case R.id.iconCamera: {
+                //Intent intentPrincipal = new Intent(MainActivity.this, CameraActivity.class);
+                //startActivity(intentPrincipal);
+                Toast.makeText(this, "Opening the camera...", Toast.LENGTH_SHORT).show();
+                openCamera();
+                break;
+            }
         }
 
         return true;
